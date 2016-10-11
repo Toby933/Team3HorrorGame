b9b2731bbe714ba5009a9731489dd4827bf38288
Christopher Atkinson <christopher.atk@ad.aie.edu>
2016-10-11 16:03:31 +1030

    More greyboxing, added a script that doesn't work. Added a black skybox material

---
+++ Assets/Scripts/Flickering_Light.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+    public GameObject lightFlicker;
+
+	// Use this for initialization
+	void Start () {
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Invoke("LightOn", 1);
+        Invoke("LightOff", 1);	
+	}
+
+    void LightOn()
+    {
+        lightFlicker.SetActive(true);
+    }
+
+    void LightOff()
+    {
+        lightFlicker.SetActive(false);
+    }
+}
