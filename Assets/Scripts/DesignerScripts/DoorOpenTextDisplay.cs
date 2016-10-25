0cce7ed13d3e5dd01c29960a68c4e6b091743f52
Christopher Atkinson <christopher.atk@ad.aie.edu>
2016-10-25 15:54:04 +1030

    Made all manor walls and floors a prefab. Created and animated a door with canvas text, with appropriate sound effect. Added canvas to all scenes.

---
+++ Assets/Scripts/DesignerScripts/DoorOpenTextDisplay.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+
+
+public class DoorOpenTextDisplay : MonoBehaviour {
+
+    public Text textBox;
+    public bool doorOpened = false;
+    private Animator doorOpenClose;
+    AudioSource doorOpenSound;
+
+	// Use this for initialization
+	void Start ()
+    {
+        doorOpenClose = GetComponentInParent<Animator>();
+        doorOpenSound = GetComponentInParent<AudioSource>();
+	}
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Player" && Input.GetKey(KeyCode.E))
+        {
+            doorOpened = true;
+            textBox.text = "";
+            Open();
+            doorOpenSound.Play();
+
+        }
+        else if (other.tag == "Player" && doorOpened == false)
+        {
+            textBox.text = "Press 'E' to open door";
+        }
+    }
+
+    void OnTriggerExit (Collider other)
+    {
+        textBox.text = "";
+    }
+
+    void Open()
+    {
+        doorOpenClose.SetTrigger("Activate");
+    }
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/DesignerScripts/TextDisplay.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+
+
+public class DooorOpenTextDisplay : MonoBehaviour {
+
+    public Text textBox;
+    public bool doorOpened = false;
+    private Animator doorOpenClose;
+
+	// Use this for initialization
+	void Start ()
+    {
+        doorOpenClose = GetComponentInParent<Animator>();
+	}
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Player" && Input.GetKey(KeyCode.E))
+        {
+            doorOpened = true;
+            textBox.text = "";
+            Open();
+        }
+        else if (other.tag == "Player" && doorOpened == false)
+        {
+            textBox.text = "Press 'E' to open door";
+        }
+    }
+
+    void OnTriggerExit (Collider other)
+    {
+        textBox.text = "";
+    }
+
+    void Open()
+    {
+        doorOpenClose.SetTrigger("Activate");
+    }
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
