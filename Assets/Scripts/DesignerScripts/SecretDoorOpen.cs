3823653708a09d13920a5326d4abfc7ab955eae5
hql180 <huyql@hotmail.com>
2016-10-31 14:40:10 +1030

    Fixed crouch alerting monster of player location Fixed movement while jumping alerting monster Fixed diagonal movement exceeding intended speeds Hid alt footstep front fps controller script Added jumping/landing sounds Added jumping/landing sounds to terrain script Footstep audio now scales with movement speed

--- Assets/Scripts/DesignerScripts/SecretDoorOpen.cs
+++ Assets/Scripts/DesignerScripts/SecretDoorOpen.cs
@@ -2,17 +2,15 @@
 using System.Collections;
 
 public class SecretDoorOpen : MonoBehaviour {
-    private AudioSource sound;
+    
     private Animator doorOpen;
     // Use this for initialization
     void Start () {
-        sound = GetComponent<AudioSource>();
-        doorOpen = gameObject.GetComponent<Animator>();
+	doorOpen= gameObject.GetComponent<Animator>();
     }
 
     void TurnOn()
     {
-        sound.Play();
         doorOpen.SetTrigger("Activate");
     }
     }
---
+++ Assets/Scripts/Test/MonsterAI/altFootStepTerrain.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class altFootStepTerrain : MonoBehaviour
+{
+    public CustomFirstPersonController player;
+
+    [Tooltip("Footstep audio")]
+    public AudioClip[] footSteps;
+
+    [Tooltip("Jump audio")]
+    public AudioClip jumpSound = null;
+
+    [Tooltip("Land audio")]
+    public AudioClip landSound = null;
+
+	// Use this for initialization
+	void Start ()
+    {
+	    
+	}
+
+    public void OnTriggerEnter(Collider collider)
+    {
+        if(collider.tag == "Player")
+        {
+            player.altFootStepAudio = footSteps;
+
+            if (jumpSound != null)
+                player.altJumpingAudio = jumpSound;
+
+            if (landSound != null)
+                player.altLandingAudio = landSound;
+        }
+    }
+
+    public void OnTriggerExit(Collider collider)
+    {
+        if (collider.tag == "Player")
+        {
+            player.altFootStepAudio = new AudioClip[0];
+
+            player.altJumpingAudio = null;
+
+            player.altLandingAudio = null;
+        }
+                
+    }
+}
