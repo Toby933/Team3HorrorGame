d8e4a0b178c7691d74b86dee8c7ec3863848884f
Tobias Kelty <tobias.kelty@ad.aie.edu>
2016-11-01 13:41:16 +1030

    Script edit, easter egg

--- Assets/Scripts/DesignerScripts/DescriptionText.cs
+++ Assets/Scripts/DesignerScripts/DescriptionText.cs
@@ -3,21 +3,21 @@ using System.Collections;
 using UnityEngine.UI;
 
 public class DescriptionText : MonoBehaviour {
-
+    public string textOutput = "Fill me in you big girl";
     public Text descriptionText;
     bool textOnScreen = false;
 
     // Use this for initialization
     void Start()
     {
-
+        descriptionText.text = "";
     }
 
     void OnTriggerStay(Collider other)
     {
         if (other.tag == "Player" && Input.GetKeyDown(KeyCode.E) && !textOnScreen)
         {
-            descriptionText.text = "Nothing of interest";
+            descriptionText.text = textOutput;
             textOnScreen = true;
         }
         else if (other.tag == "Player" && Input.GetKeyDown(KeyCode.E) && textOnScreen == true)
