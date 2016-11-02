7031af7d839571543790a593af41624ce33bf7e9
hql180 <huyql@hotmail.com>
2016-11-02 11:55:26 +1030

    Updated itemscript to highlight items or display text via dropdown menu

--- Assets/Scripts/ScriptableObject/ItemScript.cs
+++ Assets/Scripts/ScriptableObject/ItemScript.cs
@@ -1,11 +1,23 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class ItemScript : MonoBehaviour
 {
+    public enum Action { PickUp, DisplayText, DoNothing };
+
     public Item item;
 
-    public GameObject itemObject;
+    [Tooltip("Option to manually set item object")]
+    public GameObject itemObject = null;
+
+    public Action action;
+
+    private ItemBase itemActions = new ItemBase();
+
+    public Color highLightColour = new Color();
+
+    public bool useDefault = true;
 
     private Color defaultColour;
 
@@ -17,13 +29,30 @@ public class ItemScript : MonoBehaviour
 	// Use this for initialization
 	void Start ()
     {
-        itemObject = transform.parent.gameObject;
+        if (itemObject == null)
+        {
+            itemObject = transform.parent.gameObject;
+        }
+
+        if (item.textOutput == null && action == Action.DisplayText)
+        {
+            foreach (Text t in FindObjectsOfType<Text>())
+            {
+                if (t.tag == "Item")
+                    item.textOutput = FindObjectOfType<Text>();
+            }
+        }
 
         itemObject.tag = "Item";
 
         itemRenderer = transform.parent.GetComponent<Renderer>();
 
         defaultColour = itemRenderer.material.color;
+
+        if(useDefault)
+        {
+            highLightColour = new Color(102 / 255, 178 / 255, 255 / 255, .1f);
+        }
     }
 
     void OnTriggerStay(Collider other)
@@ -32,10 +61,31 @@ public class ItemScript : MonoBehaviour
         {
             if (Input.GetKey(KeyCode.E))
             {
-                other.GetComponent<CustomFirstPersonController>().inventory.Add(item);
+                switch (action)
+                {
+                    case Action.PickUp:
+                        itemActions.PickUp(other, this);
+                        break;
+                    case Action.DisplayText:
+                        itemActions.DisplayText(this);
+                        break;
+                    case Action.DoNothing:
+                        break;
+                }
 
-                itemObject.SetActive(false);
+
                 //parent.gameObject.SetActive(false);
+            }
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            if (action == Action.DisplayText)
+            {
+                itemActions.ClearText(this);
             }
         }
     }
@@ -46,10 +96,13 @@ public class ItemScript : MonoBehaviour
         if (!lookedAt)
         {
             itemRenderer.material.color = defaultColour;
+            itemRenderer.material.SetColor("_EmissionColor", new Color(0, 0, 0, 0));
+            if (action == Action.DisplayText)
+                itemActions.ClearText(this);
         }
         else
         {
-            itemRenderer.material.color = Color.blue;
+            itemRenderer.material.SetColor("_EmissionColor", highLightColour);
         }
     }
 }
