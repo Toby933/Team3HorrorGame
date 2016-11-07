1bdc2795c90e70741f4ea89cf7466ef5c13259f8
Tobias Kelty <tobias.kelty@ad.aie.edu>
2016-11-07 13:11:08 +1030

    Added radio audio to cellar and lab, made it keep playing "smoothly" between scenes. several license free jazz tracks with a bit of static backing added as well

--- Assets/Scripts/DesignerScripts/SceneChange.cs
+++ Assets/Scripts/DesignerScripts/SceneChange.cs
@@ -4,10 +4,19 @@ using UnityEngine.SceneManagement;
 
 public class SceneChange : MonoBehaviour {
     public string target;
+    public bool sendMessage = false;
+    public GameObject[] targets;
     void OnTriggerStay(Collider other)
     {
         if (other.tag == "Player")
         {
+            if (sendMessage)
+            {
+                for (int i = 0; i < targets.Length; i++)
+                {
+                    targets[i].SendMessage("TurnOn", SendMessageOptions.DontRequireReceiver);
+                }
+            }
             SceneManager.LoadScene(target);
         }
     }
