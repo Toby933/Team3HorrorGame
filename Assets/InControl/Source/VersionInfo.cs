e6d0b2285683b5f3c235f23f883e557ed3f7d9f8
DESKTOP-P3OF18H\admin <christopheratkky@gmail.com>
2016-11-09 17:53:46 +1030

    Added a portrait in the Manor and a script that upon entering a trigger in the dining room, switches the image for a scarier one and plays a creepy sound effect. Check it out for funsies

--- Assets/InControl/Source/VersionInfo.cs
+++ Assets/InControl/Source/VersionInfo.cs
@@ -29,7 +29,7 @@ namespace InControl
 				Minor = 4,
 				Patch = 4,
 
-				Build = 3955
+				Build = 3967
 
 			};
 		}
---
+++ Assets/Scripts/DesignerScripts/PortraitSwitcher.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PortraitSwitcher : MonoBehaviour {
+
+    public GameObject portraitOriginal;
+    public GameObject portraitScary;
+    public AudioSource ambientNoise;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            portraitOriginal.SetActive(false);
+            portraitScary.SetActive(true);
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
