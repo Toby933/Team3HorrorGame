b2f401c345bc996fc120eeeb6d8b510bcc220bde
hql180 <huyql@hotmail.com>
2016-11-14 14:13:55 +1030

    Made rustle trigger script Started rustler script

---
+++ Assets/Scripts/Test/Outside Scene Scripts/Rustler.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rustler : MonoBehaviour
+{
+    public AudioClip rustleAudio;
+
+    public float moveSpeed = 7;
+
+    private AudioSource rustleSource;
+
+    [HideInInspector]
+    public bool Rustle{ get; set; }
+
+	// Use this for initialization
+	void Start ()
+    {
+        rustleSource = GetComponent<AudioSource>();
+        Rustle = true;        
+        rustleSource.clip = rustleAudio;
+        rustleSource.loop = true;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Rustle && !rustleSource.isPlaying)
+        {
+            rustleSource.Play();            
+        }
+        else if(!Rustle)
+        {
+
+        }
+        else if(rustleSource.isPlaying)
+        {
+            transform.position += new Vector3(Time.deltaTime * moveSpeed, 0, 0);
+        }
+	}
+}
