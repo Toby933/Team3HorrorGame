cedb200fc7ed117903cae0bf10d3c2ca59d93a29
Wraith\Admin <tobykelty@gmail.com>
2016-11-15 12:54:06 +1030

    Collider on Monster, audio que on monster entrance

---
+++ Assets/Scripts/DesignerScripts/NoiseMaker.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NoiseMaker : MonoBehaviour {
+    private AudioSource toot;
+    private bool hasPlayed = false;
+	// Use this for initialization
+    void OnTriggerExit (Collider other)
+    {
+        if (!hasPlayed)
+        {
+            TurnOn();
+            hasPlayed = true;
+        }
+    }
+	void Start () {
+        toot = GetComponent<AudioSource>();
+	}
+    void TurnOn()
+    {
+        toot.Play();
+    }
+}
