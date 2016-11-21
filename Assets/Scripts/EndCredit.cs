17deb582538b87c58628dde719053b0942919831
hql180 <huyql@hotmail.com>
2016-11-21 15:50:39 +1030

    EndCredits should now play when you die in the lab monster can now be spawned via spawner by invoking it's spawn function

--- Assets/Scripts/EndCredit.cs
+++ Assets/Scripts/EndCredit.cs
@@ -6,10 +6,13 @@ public class EndCredit : MonoBehaviour
 {
     Animator animator;
 
+    Text text;
+
 	// Use this for initialization
 	void Start ()
     {
         animator = GetComponent<Animator>();
+        text = GetComponent<Text>();
 	}
 	
 	// Update is called once per frame
@@ -18,11 +21,13 @@ public class EndCredit : MonoBehaviour
         if (animator.GetBool("Play Credits") && animator.GetCurrentAnimatorStateInfo(0).IsName("Finished"))
         {
             Debug.Log("Finished playing");
+            text.enabled = false;
         }
 	}
 
     public void playCredits()
     {
+        text.enabled = true;
         animator.SetBool("Play Credits", true);
     }
 }
