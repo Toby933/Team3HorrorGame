2b1658a737ee1be7b10c911c0dd13de750fad7d4
hql180 <huyql@hotmail.com>
2016-11-21 13:31:02 +1030

    Added credits made monster spawner

---
+++ Assets/Scripts/EndCredit.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class EndCredit : MonoBehaviour
+{
+    Animator animator;
+
+	// Use this for initialization
+	void Start ()
+    {
+        animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (animator.GetBool("Play Credits") && animator.GetCurrentAnimatorStateInfo(0).IsName("Finished"))
+        {
+            Debug.Log("Finished playing");
+        }
+	}
+
+    public void playCredits()
+    {
+        animator.SetBool("Play Credits", true);
+    }
+}
