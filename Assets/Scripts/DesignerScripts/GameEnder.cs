51e77744cbba1c78fdff68876d2420b25e1c44e4
Tobias Kelty <tobias.kelty@ad.aie.edu>
2016-11-22 12:02:58 +1030

    GameOver and credits roll now possible, with all the bollocks that involves

---
+++ Assets/Scripts/DesignerScripts/GameEnder.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameEnder : MonoBehaviour {
+    public GameObject spawner;
+    private bool haveSpawned = false;
+    public AudioSource audio;
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player" && !haveSpawned)
+        {
+            haveSpawned = true;
+            spawner.SendMessage("spawnMonster", SendMessageOptions.DontRequireReceiver);
+            audio.Play();
+        }
+    }
+}
